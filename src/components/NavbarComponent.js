444c52b34d233cd5f47038e693fe4018f8d6464d
Rivan Haposan <rivansimans@gmail.com>
2021-11-15 16:53:00 +0700

    Reactjs, Reactsrap Installed

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarText } from "reactstrap";
+
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="success" dark expand="md">
+        <NavbarBrand href="/">Infracom-Tech</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Home</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Admin</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
